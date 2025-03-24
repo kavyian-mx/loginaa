5348c258a5adf6a7e1e3def58da649a8e97e9c98
Matin <Matin@DESKTOP-QUGOMAD>
2025-03-24 20:28:43 +0330

    debug2

--- obj/Debug/net9.0/samplematin.AssemblyInfo.cs
+++ obj/Debug/net9.0/samplematin.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("samplematin")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+786215aceac6bf1ea21e73e9f60a0be22bcd8558")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a5d8f1eb708982421fa207ba3a334392430173ef")]
 [assembly: System.Reflection.AssemblyProductAttribute("samplematin")]
 [assembly: System.Reflection.AssemblyTitleAttribute("samplematin")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
